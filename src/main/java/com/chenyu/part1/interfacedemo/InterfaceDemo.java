8b3ab8b5ac004472dc90d15ae2532b75c2e4b8cf
chenyu <772009202@qq.com>
2021-05-17 09:13:29 +0800

    day2：基于注解的方式 解析sql

--- src/main/java/com/chenyu/part1/interfacedemo/InterfaceDemo.java
+++ src/main/java/com/chenyu/part1/interfacedemo/InterfaceDemo.java
@@ -1,6 +1,16 @@
 package com.chenyu.part1.interfacedemo;
 
+import com.google.common.collect.Maps;
+import com.mysql.cj.util.StringUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Map;
+
 /**
+ * 模拟<br>
  * @Select() 这种方式去读取sql 并执行
  *
  * @author chenyu
@@ -8,12 +18,61 @@ package com.chenyu.part1.interfacedemo;
  */
 public class InterfaceDemo {
   public static void main(String[] args) {
-    User user = new User(1, "");
-    user.getId();
+    // step1:通过jdk动态代理获取新对象
+    SelectDemo select =
+        (SelectDemo)
+            Proxy.newProxyInstance(
+                InterfaceDemo.class.getClassLoader(),
+                new Class[] {SelectDemo.class},
+                (Object proxy, Method method, Object[] methodArgs) -> {
+                  Select selectAnnotation = method.getAnnotation(Select.class);
+                  if (selectAnnotation != null) {
+                    String sqlStr = selectAnnotation.value();
+                    if (!StringUtils.isNullOrEmpty(sqlStr)) {
+                      Map<String, Object> paramMap = getParamMapByArgs(method, methodArgs);
+                      String executeSql = parseSqlByParam(sqlStr, paramMap);
+                      System.out.println(executeSql);
+                    }
+                  }
+
+                  return null;
+                });
+    select.select(1, "chenyu");
+  }
+
+  /**
+   * 将method参数名作为key，arg作为value，去sql中替换
+   *
+   * @param method
+   * @param methodArgs
+   * @return
+   */
+  public static Map<String, Object> getParamMapByArgs(Method method, Object[] methodArgs) {
+    Parameter[] parameters = method.getParameters();
+    Map<String, Object> paramMap = Maps.newHashMap();
+    int[] index = {0};
+    Arrays.asList(parameters)
+        .forEach(
+            parameter -> {
+              String name = parameter.getName();
+              paramMap.put(name, methodArgs[index[0]]);
+              index[0]++;
+            });
+    return paramMap;
+  }
+
+  public static String parseSqlByParam(String sql, Map<String, Object> paramMap) {
+    // 这里用替换的方式去做 不好 准备参考mybatis 通过字符比对去做 -- 待优化
+    for (String e : paramMap.keySet()) {
+      String replaceStr = "#{" + e + "}";
+      sql = sql.replace(replaceStr, paramMap.get(e) == null ? "" : paramMap.get(e).toString());
+    }
+
+    return sql;
   }
 }
 
 interface SelectDemo {
-  //  @Select("select * from user where id = #{id} and name = #{name}")
+  @Select("select * from user where id = #{id} and name = #{name}")
   User select(int id, String name);
 }
--- src/main/java/com/chenyu/part1/interfacedemo/Select.java
+++ src/main/java/com/chenyu/part1/interfacedemo/Select.java
@@ -3,11 +3,12 @@ package com.chenyu.part1.interfacedemo;
 import java.lang.annotation.*;
 
 /**
+ * 自定义注解 模拟mybatis注解 <br>
  * Created by chenyu on 2021/5/13
  *
  * @author chenyu
  */
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Select {
